b0d7d18d59460ccb9f00ddf264327eb94a79435c
Achille Pénet <achille.penet@icloud.com>
2018-01-04 21:33:11 +0100

    Bv0.1

--- daemon/data_daemon.js
+++ daemon/data_daemon.js
@@ -3,9 +3,11 @@ const MongoClient = require('mongodb').MongoClient
 , assert = require('assert');
 
 var json
-var request_url = 'https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=position-des-bus-en-circulation-sur-le-reseau-star-en-temps-reel&facet=numerobus&facet=etat&facet=nomcourtligne&facet=sens&facet=destination&refine.nomcourtligne=C4'
+var request_url = 'https://data.explore.star.fr/api/records/1.0/search/?dataset=tco-bus-vehicules-position-tr&facet=numerobus&facet=etat&facet=nomcourtligne&facet=sens&facet=destination&refine.nomcourtligne=C4&refine.sens=1'
 // Connection URL
 var url = 'mongodb://mongodb:27017/bus_data';
+
+var coordinate = [[48.1103, -1.68039],[48.1103, -1.68039]];
 
 //Use connect method to connect to the server and create a new collection
 MongoClient.connect(url, function(err, db) {
@@ -14,6 +16,11 @@ MongoClient.connect(url, function(err, db) {
   db.createCollection("coordinates", function(err, res) {
     if (err) throw err;
     console.log("Collection created!");
+    var myquery = {};
+    db.collection("coordinates").insert(myquery, {coordinate}, function(err, res) {
+      if (err) throw err;
+      console.log("1 document updated");
+    });
     db.close();
   });
   console.log("Successfully connected to the database !")
@@ -24,19 +31,23 @@ var data_request = function () {
   request(request_url, function (error, response, body) {
     console.log('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    json = body
-    console.log('Bus C4 : ' + JSON.parse(json))
-    //Use connect method to connect to the server
-    MongoClient.connect(url, function(err, db) {
-      var myquery = {};
-      var newvalues = JSON.parse(json);
-      db.collection("coordinates").updateOne(myquery, newvalues, function(err, res) {
-        if (err) throw err;
-        console.log("1 document updated");
-        db.close();
+    json = JSON.parse(body)
+    console.log(json.records[0].fields.coordonnees)
+    if(coordinate[1] != json.records[0].fields.coordonnees){
+      coordinate[0] = coordinate[1];
+      coordinate[1] = json.records[0].fields.coordonnees;
+      //Use connect method to connect to the server
+      MongoClient.connect(url, function(err, db) {
+        var myquery = {};
+        db.collection("coordinates").updateOne(myquery, {coordinate}, function(err, res) {
+          if (err) throw err;
+          console.log("1 document updated");
+          db.close();
+        });
       });
-    });
+    }
+    console.log({coordinate})
   });
 }
+data_request()
 setInterval(data_request, 30*1000)
-//data_request();
--- server/server.js
+++ server/server.js
@@ -4,10 +4,15 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient
 , assert = require('assert');
 const path = require('path');
+const request = require('request');
+
+var coordinate = [[48.1103, -1.68039],[48.1103, -1.68039]];
 
 var url = 'mongodb://mongodb:27017/bus_data';
 
 app.use('/MovingMarker.js', express.static(path.join(__dirname, '/MovingMarker.js')));
+app.use('/index.js', express.static(path.join(__dirname, '/index.js')));
+app.use('/icon/bus.png', express.static(path.join(__dirname, '/icon/bus.png')));
 
 app.get('/', function (req, res) {
   console.log("Reached by client");
@@ -15,20 +20,32 @@ app.get('/', function (req, res) {
 });
 
 app.get('/data', function(req, res) {
-  var getData = function(){
-    MongoClient.connect(url, function(err, db) {
-      db.collection("coordinates").find(function(err, docs) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.log(docs.records[0].fields.coordonnees);
-        res.send(docs.records[0].fields.coordonnees);
-      });
-    });
-  }
-  setInterval(getData, 30*1000)
-  res.send("Yalla");
+    res.send(coordinate)
 });
 
 app.listen(3080, function(){
   console.log('listening on :3080');
 });
+
+// Request the data to the server and save them in the database
+var data_request = function () {
+  MongoClient.connect(url, function(err, db) {
+    var cursor = db.collection('coordinates').find();
+    // Execute the each command, triggers for each document
+    cursor.each(function(err, item) {
+        // If the item is null then the cursor is exhausted/empty and closed
+        if(item == null) {
+            db.close()
+            return;
+        }else{
+          console.log("#################### item");
+          console.log(item.coordinate);
+          coordinate = item.coordinate;
+        }
+        // otherwise, do something with the item
+    });
+  });
+  console.log(coordinate);
+}
+data_request()
+setInterval(data_request, 10*1000)
